8087d8a89c2e07e34d9f61435d1af714ec488033
ilecipi <ilecipi@hotmail.com>
2018-06-03 16:31:48 +0200

    added writer to xlsx file

--- DataAnalysisTweets.py
+++ DataAnalysisTweets.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas import ExcelWriter
+from pandas import ExcelFile
+import xlsxwriter
 
-data = pd.read_csv('ResultDATA.csv')
-
+data = pd.read_csv('secondresult_clean.csv', sep=';')
+print(data)
 group_tweet = data.groupby(['_unit_id','date'])['how_would_you_categorize_this_tweet'].mean().reset_index()
 group_polarity = data.groupby(['_unit_id','date'])['polarity'].mean().reset_index()
 group_google = data.groupby(['_unit_id','date'])['score_google'].mean().reset_index()
@@ -18,3 +21,7 @@ group_per_day_google = data.groupby(['date'])['score_google'].mean().reset_index
 result = pd.merge(group_per_day_tweet, group_per_day_polarity, on='date', how='inner')
 result2 = pd.merge(result, group_per_day_google, on='date', how='inner')
 print(result2)
+
+writer = pd.ExcelWriter('FinalResults.xlsx', engine='xlsxwriter')
+result2.to_excel(writer, sheet_name='Sheet1')
+writer.save()
